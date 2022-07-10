08796476b4bddc4ab3b2bfd213f27956beeebacd
taek0622 <min4644@gmail.com>
2022-07-10 15:07:34 +0900

    Feat✨: Add HistoryView

---
+++ EnglishWordGenerator/HistoryView.swift
@@ -0,0 +1,22 @@
+//
+//  HistoryView.swift
+//  EnglishWordGenerator
+//
+//  Created by 김민택 on 2022/07/10.
+//
+
+import SwiftUI
+
+struct HistoryView: View {
+    var body: some View {
+        VStack {
+            Text("History")
+        }
+    }
+}
+
+struct HistoryView_Previews: PreviewProvider {
+    static var previews: some View {
+        HistoryView()
+    }
+}
